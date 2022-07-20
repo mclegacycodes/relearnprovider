5e18662af1271ca99fc80382f41cb1f0c2824e4e
unknown <kingsleytwum2@gmail.com>
2022-07-20 15:09:51 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MainPage(),
+    );
+  }
+}
+
+class MainPage extends StatelessWidget {
+  const MainPage({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    print('Building MainPage');
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Data Here'),
+      ),
+      body: const Screen2(),
+    );
+  }
+}
+
+class Screen2 extends StatelessWidget {
+  const Screen2({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    print('Building Screen2');
+    return Container(
+      child: const Screen3(),
+    );
+  }
+}
+
+class Screen3 extends StatelessWidget {
+  const Screen3({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    print('Building Screen3');
+    return Container(
+      child: const Screen4(),
+    );
+  }
+}
+
+class Screen4 extends StatelessWidget {
+  const Screen4({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    print('Building Screen4');
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Data Here'),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Change data'),
+          ),
+        ],
+      ),
+    );
+  }
+}
